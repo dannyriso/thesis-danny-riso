a78f06132f3680293866b576c72a40df09c1988b
Danny Riso <dariso@reed.edu>
2020-11-19 15:42:02 -0500

    Adds print statement of all instances by subcategory

--- wiki_demo.py
+++ wiki_demo.py
@@ -31,13 +31,13 @@ def findPagesInCategory(catlinks_filename, desired_category):
 
 def findPagesById(page_filename, id_list):
     page_file = open(page_filename, 'r')
-    pages_in_category = []
+    pages_in_category = dict()
     
     for page in page_file:
         page_id, page_namespace, page_title, page_is_redirect, page_len, page_content_model, page_lang = page.split('\t')
         
         if page_id in id_list:
-            pages_in_category.append(page_title)
+            pages_in_category[page_title] = [page_id, page_namespace]
             id_list.remove(page_id)
     
     page_file.close()
@@ -46,31 +46,55 @@ def findPagesById(page_filename, id_list):
 def main():
     
     # Initialize variables for current search
-    
+    my_dump_date = "20201020"
+        
     # To test with enwiki, finding all Dog Breed pages:
     # (https://en.wikipedia.org/wiki/Category:Dog_breeds)
-    my_dump_date = "20201020"
-    wiki = "enwiki"
-    desired_category = "'Dog_breeds'"
+    # wiki = "enwiki"
+    # desired_category = "'Dog_breeds'"
 
-    """
     # To test with enwikibooks, smaller set with fewer subcategories:
     # (https://en.wikibooks.org/wiki/Category:Book:Organic_Chemistry)
     wiki = "enwikibooks"
-    desired_category = "'Book:Organic_Chemistry'"
-    """
+    # For finding many pages:
+    # desired_category = "'Organic_Chemistry'"
+    # For devising a taxonomy from many subcategories:
+    desired_category = "'Recipes'"
     
     catlinks_filename = wiki + "-" + my_dump_date + "-categorylinks"
     pages_filename = wiki + "-" + my_dump_date + "-page"
     
-    # Call desired methods
+    # Collect data from txt files
     page_ids, subcat_ids, file_ids = findPagesInCategory(catlinks_filename, desired_category)
     pages = findPagesById(pages_filename, page_ids)
+    subcats = findPagesById(pages_filename, subcat_ids)
+    files = findPagesById(pages_filename, file_ids)
     
     # Print results for debugging and clarity
-    print("Pages in category", desired_category, ":")
+    print ("Pages in category", desired_category, ":")
     for p in pages:
         print(p)
+    print("Subcategories in category", desired_category, ":")
+    for s in subcats:
+        print(s)
+    print("Files in category", desired_category, ":")
+    for f in files:
+        print(f)
+    
+    """
+    # Prints terminal children by subcategory
+    while(subcats):
+        sub = list(subcats.keys())[0]
+        page_ids, subcat_ids, file_ids = findPagesInCategory(catlinks_filename, sub)
+        pages = findPagesById(pages_filename, page_ids)
+        subs = findPagesById(pages_filename, subcat_ids)
+        subcats.update(subs)
+        print()
+        print("Kids of", sub, ":")
+        for p in pages:
+            print(p)
+        subcats.pop(sub)
+    """
     
     return 0
 
